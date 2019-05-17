7f2dfe99b34899de72b002aa1c5372460a1a8fa1
Kaihil Patel <PatelKN20@pwcs-edu.org>
2019-05-17 10:46:09 -0400

    kaihils orignial project

---
+++ PersonC/src/PersonC.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class PersonC {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		
+	    Scanner scanner = new Scanner(System.in)
+	    System.out.print("Type your first number");
+		int x = scannnnnnner.nextInt();
+		System.out.print("Type your second number");
+		int x = scanner.nextNumber();
+		System.out.print("here is the random number:" +  (int) Math.random()*x + y);
+
+	}
+
+}
